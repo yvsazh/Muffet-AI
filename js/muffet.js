110fb89a22f162c67bbac85a8d9b7aba77eb2ee9
Hackwhizzz <77292802+yvsazh@users.noreply.github.com>
2024-11-14 22:51:29 +0200

    fixed all bugs

--- js/muffet.js
+++ js/muffet.js
@@ -1,7 +1,7 @@
 class Muffet {
     constructor(x, y) {
         this.sprite = new Sprite(x, y, 200, 200, "n");
-        this.sprite.addAnimation("idle", '/assets/muffet/img0.png', 34);
+        this.sprite.addAnimation("idle", '/Muffet-AI/assets/muffet/img0.png', 34);
         this.sprite.animation.scale = 1.3;
     }
 }
